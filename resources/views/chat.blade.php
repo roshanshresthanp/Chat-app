8189986770073a906b6a47dcd8fa848d47e44d1e
roshan <stha901@gmail.com>
2021-05-28 17:23:27 +0545

    Adding message component and changes in views

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -1,17 +1,32 @@
 <html>
     <head><title>ChatApp</title>
 
-        {{-- <meta name="csrf-token" content="{{csrf_token() }}" > --}}
+        <meta name="csrf-token" content="{{csrf_token() }}" >
         <link rel="stylesheet" href="{{asset('css/app.css')}}" type="text/css">
     {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> --}}
     
+        
     </head>
+    <style>
+        .list-group{
+            overflow-y: scroll;
+            height: 200px;
+        }
+    </style>
     <body>
         <div class="container">
             <div class="row" id="app">
                 <div class="offset-4 col-4">
                     <li class="list-group-item active">Chat room</li>
-                      <input type="text" class="form-control" placeholder="Type your message here..." @keyup.enter='send'>
+
+                    <ul class="list-group">
+                    <message v-for="value in chat.message">
+                        @{{value}}
+                    </message>
+                    </ul>
+                    
+                      <input type="text" class="form-control" placeholder="Type your message here..." 
+                      v-model="message" @keyup.enter='send'>
                 </div>
                 
 
