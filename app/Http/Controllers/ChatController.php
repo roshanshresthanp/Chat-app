e9754d5abb06ca8670fa7bd863303d8267f56a04
roshan <stha901@gmail.com>
2021-05-29 21:16:08 +0545

    Event to broadcast is created

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -6,8 +6,19 @@ use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function chat()
     {
         return view('chat');
+    }
+    public function send(request $request)
+    {
+        $user = Auth::user()->id;
+        event (new ChatEvent($message, $user));
     }
 }
